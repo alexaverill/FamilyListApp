a01822aa62b2e89c790b49dd5d563df253a020ee
Alex <alex.averill2013@gmail.com>
2020-09-06 14:29:15 -0600

    chore: working on integration of typeorm

--- familylistapp/pages/api/test.js
+++ familylistapp/pages/api/test.js
@@ -1,15 +1,17 @@
+
 import {createConnection} from 'typeorm';
 import {User} from '../../entities/user'
 import { create } from 'domain';
 
-export default (req, res) => {
+export default async function (req, res) {
+    
+    createConnection().then(async connection =>{
+        const testUser = new User();
+        testUser.name = "Test";
+        testUser.email = "test@test.com";
+        await connection.manager.save(testUser);
+    }).catch(error =>{console.log(error)});
     res.statusCode = 200
-  res.json({ name: 'John Doe' })
-    // createConnection().then(async connection =>{
-    //     const testUser = new User();
-    //     testUser.name = "Test";
-    //     testUser.email = "test@test.com";
-    //     await connection.manager.save(testUser);
-    // });
+    res.json({ name: 'John Doe' })
   }
   
