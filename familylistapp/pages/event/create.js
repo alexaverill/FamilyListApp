ff979f4d7515f17abbd09370731dad09e7db2d89
Alex <alex.averill2013@gmail.com>
2020-09-07 15:17:42 -0600

    feat: setting up placeholder create pages

---
+++ familylistapp/pages/event/create.js
@@ -0,0 +1,10 @@
+import React from 'react';
+export default class CreateEvent extends React.Component{
+    render(){
+        return(
+            <>
+            <h1>I am a create Page!</h1>
+            </>
+        )
+    }
+}
