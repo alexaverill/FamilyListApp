ff24bb3de2bc43599160b4e53e81d9f7e98383cb
Alex <alex.averill2013@gmail.com>
2020-09-07 15:14:46 -0600

    feat: transfering over homepage view

---
+++ familylistapp/views/HomeView.js
@@ -0,0 +1,47 @@
+import React from 'react';
+import Container from 'react-bootstrap/Container';
+import Button from 'react-bootstrap/Button';
+import EventCard from '../components/EventCard.js';
+import Col from 'react-bootstrap/Col';
+import Row from 'react-bootstrap/Row';
+import {getAllEvents} from './API.js';
+class HomeCard{
+    constructor(id,date,title){
+        this.id = id;
+        this.date = date;
+        this.title = title;
+    }
+}
+class HomeView extends React.Component{
+    constructor(props){
+        super(props);
+        this.state={events:[]};
+    }
+    componentDidMount(){
+        getAllEvents().then(data=>{
+            this.setState({events:data});
+        });
+    }
+
+    render(){
+        const cards = this.state.events.map((e)=> <EventCard date={new Date(e.eventDate)} title={e.eventName} id={e.id} image={e.image}/>);
+
+
+        return(
+            <Container className="innerContent">
+            <div className="homeHeader">
+                <div className="headerText">
+                    <h2>Events</h2>
+                </div>
+                <div className="">
+                    <a href="/createEvent" className="btn btn-primary addEventBtn">Add Event</a>
+                </div>
+            </div>
+            <Row>
+                {cards}
+            </Row>
+            </Container>
+        );
+    }
+}
+export default HomeView;
