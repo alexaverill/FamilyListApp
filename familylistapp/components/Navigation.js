605392cd6e093a4083bd0a9e1e2df3f92be62c16
Alex <alex.averill2013@gmail.com>
2020-09-12 12:58:02 -0600

    chore: fixing bootrap css dependencies

---
+++ familylistapp/components/Navigation.js
@@ -0,0 +1,41 @@
+import React from 'react';
+import Navbar from 'react-bootstrap/Navbar';
+import Nav from 'react-bootstrap/Nav';
+import NavDropdown from 'react-bootstrap/NavDropdown';
+//import {getUsername} from './Session'
+import Container from 'react-bootstrap/Container';
+class Navigation extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+    render() {
+        //let name = getUsername();
+        let name = "";
+        let showLogin = false;
+        // if(name == null || name.length <=0){
+        //     showLogin = true;
+        // }
+        return (
+            
+            <Navbar expand="lg">
+                <Container>
+                <Navbar.Brand href="/">List App</Navbar.Brand>
+                <Navbar.Toggle aria-controls="basic-navbar-nav" />
+                <Navbar.Collapse id="" className="justify-content-end">
+                    {/* <Nav className="mr-auto">
+                        <Nav.Link href="/">Events</Nav.Link>
+                        <Nav.Link href="/">Link</Nav.Link>
+                    </Nav> */}
+                    <Nav>
+                        {showLogin ? <Nav.Link href="/login">Login</Nav.Link>:
+                        <NavDropdown title={name} id="basic-nav-dropdown">
+                            <NavDropdown.Item href="logout">Logout</NavDropdown.Item>
+                        </NavDropdown>}
+                    </Nav>
+                </Navbar.Collapse>
+                </Container>
+            </Navbar>
+        );
+    }
+}
+export default Navigation;
--- familylistapp/pages/_app.js
+++ familylistapp/pages/_app.js
@@ -1,7 +1,15 @@
 import '../styles/globals.css'
-
+import 'bootstrap/dist/css/bootstrap.min.css';
+import Navigation from '../components/Navigation';
+import Container from 'react-bootstrap/Container';
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  
+  return <>
+  <Container>
+    <Navigation/>
+  <Component {...pageProps} />
+  </Container>
+  </>
 }
 
 export default MyApp
