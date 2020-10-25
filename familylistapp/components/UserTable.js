1d8526a03915be52cde59fd53c34ea9121fa31ad
Alex <alex.averill2013@gmail.com>
2020-10-25 13:56:59 -0600

    feat: framing out create user form

--- familylistapp/components/UserTable.js
+++ familylistapp/components/UserTable.js
@@ -1,8 +1,7 @@
 import React from 'react';
-import Container from 'react-bootstrap/Container';
-import Button from 'react-bootstrap/Button';
-import Card from 'react-bootstrap/Card';
+import Row from 'react-bootstrap/Row';
 import Table from 'react-bootstrap/Table';
+import Button from 'react-bootstrap/Button'
 import {AuthGetRequest, AuthPostRequest,AuthDeleteRequest} from '../utils/api';
 import { getKey } from '../utils/session';
 import EditableInput from './EditableInput'
@@ -110,14 +109,27 @@ class UserTable extends React.Component{
                          onChangeHandle={this.handleChangedEmail}
                          onFinish = {this.handleUserUpdate}/></td>
             <td>
+                <Row>
                 <ResetPassword text={user.password} id={user.id} onChangeHandle={this.handleChangePassword}
                                 onFinish={this.handleUserUpdate}/> 
-                <button className="btn btn-danger" onClick={()=>{this.deleteUser(user.id);}}>Delete</button>
+                <button className="btn btn-danger" onClick={()=>{this.deleteUser(user.id);}}>
+                    <svg width="1em" height="1em" viewBox="0 0 16 16" class="bi bi-trash" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                    <path d="M5.5 5.5A.5.5 0 0 1 6 6v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm2.5 0a.5.5 0 0 1 .5.5v6a.5.5 0 0 1-1 0V6a.5.5 0 0 1 .5-.5zm3 .5a.5.5 0 0 0-1 0v6a.5.5 0 0 0 1 0V6z"/>
+                    <path fill-rule="evenodd" d="M14.5 3a1 1 0 0 1-1 1H13v9a2 2 0 0 1-2 2H5a2 2 0 0 1-2-2V4h-.5a1 1 0 0 1-1-1V2a1 1 0 0 1 1-1H6a1 1 0 0 1 1-1h2a1 1 0 0 1 1 1h3.5a1 1 0 0 1 1 1v1zM4.118 4L4 4.059V13a1 1 0 0 0 1 1h6a1 1 0 0 0 1-1V4.059L11.882 4H4.118zM2.5 3V2h11v1h-11z"/>
+                    </svg>
+                </button>
+                </Row>
             </td>
                                 
           </tr>;
         })
         return (
+
+            <>
+            <Row><Button onClick={this.addUser}>Add User</Button></Row>
+            <Row>
+                
+            </Row>
             <Table striped bordered hover>
   <thead>
     <tr>
@@ -130,6 +142,7 @@ class UserTable extends React.Component{
             {userTable}
             </tbody>
             </Table>
+            </>
         );
     }
 }
