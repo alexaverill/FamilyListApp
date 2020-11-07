9a69ac8e6e1b8faba948aa2ca3e4a10367718710
Alex <alex.averill2013@gmail.com>
2020-11-07 13:09:46 -0700

    feat: adding in custom favicon

--- familylistapp/pages/api/AuthMiddleware.js
+++ familylistapp/pages/api/AuthMiddleware.js
@@ -9,3 +9,15 @@ export function AuthMiddleware(req,res){
         return resolve(false);
     })
 }
+export function AdminAuthMiddleware(req,res){
+    let token = req.headers.authorization;
+    //console.log("TOKEN: "+token);
+    return new Promise((resolve,reject) =>{
+        let userObj = tokenizer.getInfoFromToken(token);
+        if("username" in userObj && "isAdmin" in userObj){
+            resolve(userObj.isAdmin);
+        }else{
+            resolve(false);
+        }
+    })
+}
--- familylistapp/pages/index.js
+++ familylistapp/pages/index.js
@@ -7,6 +7,12 @@ export default function Home() {
         <Head>
         <title>List App</title>
         <link rel="icon" href="/favicon.ico" />
+        <link rel="apple-touch-icon" sizes="57x57" href="apple-touch-icon.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="android-chrome-192x192.png"/>
+        <link rel="icon" type="image/png" sizes="192x192"  href="android-chrome-512x512.png"/>
+        <link rel="icon" type="image/png" sizes="32x32" href="favicon-32x32.png"/>
+        <link rel="icon" type="image/png" sizes="16x16" href="favicon-16x16.png"/>
+
       </Head>
       <HomeView/>
     </>
